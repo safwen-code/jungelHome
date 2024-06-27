875cb74a9e272091f9b10537ffdcdcc206b19955
safwen-code <safwendjebbi1234@gmail.com>
2024-06-27 12:28:37 +0100

    new navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import { Box, Typography } from '@mui/material'
+import logo1 from '../assets/logo1.png'
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <Box
+      sx={{
+        color: 'black',
+        textAlign: 'right',
+        padding: 4,
+        borderBottom: 'solid 1px black',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        component="img"
+        src={logo1}
+        alt="logo"
+        sx={{
+          height: 45,
+          width: 45,
+        }}
+      />
+      <Typography
+        variant="h6"
+        sx={{
+          paddingLeft: 4,
+        }}
+      >
+        Jungel Home
+      </Typography>
+    </Box>
+  )
 }
 
 export default Navbar
