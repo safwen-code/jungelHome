647ad99a97d7236dc83a54338ba5d34be1ae050e
safwen-code <safwendjebbi1234@gmail.com>
2024-06-29 12:28:37 +0100

    new update

--- src/Components/Acceuil.jsx
+++ src/Components/Acceuil.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Container } from '@mui/material'
+import Navbar from './Navbar'
+import Content from './Content'
 
 const Acceuil = () => {
-  return <div>Acceuil</div>
+  return (
+    <Container maxWidth="xl">
+      <Navbar />
+      <Content />
+    </Container>
+  )
 }
 
 export default Acceuil
