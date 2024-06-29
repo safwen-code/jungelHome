ec3d25185ce2ce0eb4da5a79708e3fd8b3eceeae
safwen-code <safwendjebbi1234@gmail.com>
2024-06-29 12:52:35 +0100

    new update to finish categories thing

--- src/Components/ShoppingCarts.jsx
+++ src/Components/ShoppingCarts.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
   Card,
@@ -11,8 +11,15 @@ import {
 } from '@mui/material'
 
 import { plantList } from '../data/PlantList'
+import Categori from './Categori'
 
 const ShoppingCarts = ({ card, setcard }) => {
+  const [activeCategory, setactiveCategory] = useState('')
+  const categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    [],
+  )
   const hundelFavories = (name, price) => {
     const currentPlantSaved = card.find((plant) => plant.name !== name)
     if (currentPlantSaved) {
@@ -26,35 +33,42 @@ const ShoppingCarts = ({ card, setcard }) => {
     }
   }
   return (
-    <Grid container spacing={2} mt={2}>
-      {plantList.map((plant) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={plant.id}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image={plant.cover}
-                alt={plant.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {plant.name}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  onClick={() => hundelFavories(plant.name, plant.price)}
-                >
-                  add favorie
-                </Button>
-              </CardActions>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Categori
+        categories={categories}
+        setactiveCategory={setactiveCategory}
+        activeCategory={activeCategory}
+      />
+      <Grid container spacing={2} mt={2}>
+        {plantList.map((plant) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={plant.id}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={plant.cover}
+                  alt={plant.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {plant.name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    onClick={() => hundelFavories(plant.name, plant.price)}
+                  >
+                    add favorie
+                  </Button>
+                </CardActions>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
   )
 }
 
