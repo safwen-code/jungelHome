fa510092cd37f62b5001db23540eaad2d60bffdc
safwen-code <safwendjebbi1234@gmail.com>
2024-06-29 14:57:52 +0100

    change Content

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Pannier from './Pannier'
+import ShoppingCarts from './ShoppingCarts'
+import { Box } from '@mui/material'
 
 const Content = () => {
-  return <div>Content</div>
+  const savedCard = localStorage.getItem('card')
+  const [card, setcard] = useState(savedCard ? JSON.parse(savedCard) : [])
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('card', JSON.stringify(card))
+  }, [card])
+
+  useEffect(() => {
+    setIsOpen(card.length > 0)
+  }, [card])
+
+  return (
+    <Box display="grid">
+      <Pannier
+        card={card}
+        setcard={setcard}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+      <ShoppingCarts card={card} setcard={setcard} isOpen={isOpen} />
+    </Box>
+  )
 }
 
 export default Content
